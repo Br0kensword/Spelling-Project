99491beee911a5321140ca839564d55a4b69cd45
Br0kensword <longislandhustler@gmail.com>
2015-08-24 17:04:31 -0700

    added new hebrew lists as well as an updated hebrew spellcheck

--- js/spellScript.js
+++ js/spellScript.js
@@ -1,13 +1,21 @@
-var quiz1 = ['ששש','doge','quail','cappy'];
-
-var quiz2 = ['pigeon','hyrax','piglet','snugglefish'];
-
-var quiz3 = [['להימצא', 'be located at'], ['להוסיף', 'add'], ['להבין', 'understand'], ['להתערב', 'interfere'], ['להפסיק', 'stop doing STH'], ['להפוך', 'turn in sideup', 'upside down']
+var quiz1 = [['להימצא', 'be located at'], ['להוסיף', 'add'], ['להבין', 'understand'], ['להתערב', 'interfere'], ['להפסיק', 'stop doing STH'], ['להפוך', 'turn in sideup', 'upside down']
               , ['לחקות', 'imitate'], ['לנסות', 'try'], ['להיכנס', 'enter'], ['לפנות', 'turn to'], ['לפסול', 'disqualify'], ['להיגמר', 'come to an end'], ['להרגיז', 'annoy'], ['להסתובב', 'walk around']
               , ['להשחית', 'destroy'], ['לזכות', 'merit'], ['לספר', 'tell a story', 'recount']];
 
+var quiz2 = [ ['אקונומיקה', 'bleach'], ['מלחמות', 'war'], ['מכירת סוף העונה', 'sale', 'end of season sale'], ['מלאה', 'full'], ['הכשר', 'Kosher certification'], ['נגד','against']
+              , ['אישה', 'woman'], ['ישן', 'things'], ['רשימה', 'list'], ['לוח מודעות', 'לוחות מודעות ','billboard'], ['עיר', 'ערים', 'city'], ['איסור', 'איסורים ','prohibition'], ['פעוּלה', 'פעוּלות', 'action', 'activity']
+              , ['קורבָּן','victim'], ['הודעה', 'הודעות', 'announcement'], ['מרוּצה','happy','content'] ];
 
-var wordList = quiz3;
+var quiz3 = [['גופייה', 'גופיות', 'undershirt'], ['מכנסיים','pants'], ['צמוד', 'צמודה','tight'], ['חולצה', 'חולצות',  'shirt', 'blouse'], ['שמלה', 'שמלות', 'dress'], ['חנות', 'חנויות',  'store']
+              , ['שרווּל', 'שרווּלים','sleeve'], ['אורך', 'length'], ['לבוש','garment', 'attire'], ['גזרה', 'גזרות', 'shape of garment'], ['סוּג', 'סוּגים','kind'], ['בד', 'בדים', 'material']
+              , ['סריג', 'סריגים', 'knit'], ['קצר', 'קצרה','short'],['מתאים', 'מתאימה', 'appropriate fit']];
+
+var quiz4 = [['נציג', 'נציגים', 'representative'], ['דרישה', 'דרישות',  'demand'], ['מוּגזם', 'מוּגזמת','exaggerated'], ['עשוּי', 'עשוּייה','made of'], ['ברך', 'ברכיים','knee'], ['צניעוּת', 'modesty']
+              , ['הציבוּר החרדי', 'the Xaredi public'], ['מלחמת קודש',  'holy war'], ['חילוני', 'חילונית',  'secular person'], ['מעצב', 'מעצבת','designer'], ['איזור', 'איזורים','area'], ['השפעה','influence']
+              , ['מסר','message'], ['אמור', 'אמורה','supposed to'],['שמרני', 'שמרנית  ','conservative']];
+
+
+var wordList = quiz1;
 
 var wrongList = [];
 var customWords = [];
@@ -18,35 +26,52 @@ var wrongAmount = wrongList.length;
 
 $('#list1').click( function() {
   wordList = quiz1;
-  
   currWord = 0;
-  
   wrongList = [];
+
   $("#wrong").html(wrongAmount);
-  alert(quiz3[0]);
+  $('#instruct').html(wordList[currWord][wordList[currWord].length - 1]);
 });
 
 $('#list2').click( function() {
   wordList = quiz2;
-  
   currWord = 0;
-  
   wrongList = [];
+
   $("#wrong").html(wrongAmount);
-  alert("Loading Quiz 2");
+  $('#instruct').html(wordList[currWord][wordList[currWord].length - 1]);
 });
 
+$('#list3').click( function() {
+  wordList = quiz3;
+  currWord = 0;
+  wrongList = [];
 
+  $("#wrong").html(wrongAmount);
+  $('#instruct').html(wordList[currWord][wordList[currWord].length - 1]);
+});
+
+$('#list4').click( function() {
+  wordList = quiz4;
+  currWord = 0;
+  wrongList = [];
+
+  $("#wrong").html(wrongAmount);
+  $('#instruct').html(wordList[currWord][wordList[currWord].length - 1]);
+});
+
+//current if statement is only set up for hebrew spellcheck!!!!
 $( "#subButton" ).click(function() {
     alert(wordList[currWord][0]);
-    if ( $( "#answerBox" ).val() == wordList[currWord][0] ) {
+    if ( $( "#answerBox" ).val() == wordList[currWord][0].split('').reverse().join('') ) {
       $("#my_span").css('color','green');
       $("#my_span").html('correct');
       
       
       currWord += 1;
       $("#right").html(wordList.length);
-      
+      $('#instruct').html(wordList[currWord][1]);
+
       if(currWord == wordList.length) {
         if (wrongList.length != 0){
           wordList = wrongList;
@@ -140,7 +165,7 @@ $('#customSubmit').click( function() {
 });
 
 
-$("#right").html(wordList.length);
+$("#right").html("Total Words: " + wordList.length);
 
 
 
